d3ab4c5c23ead335cc181e2ed384492ab6c6b217
Naveen Kumar A M <amnaveenkumar123@gmail.com>
2024-11-29 12:10:12 +0530

    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { Invoices } from "./schema";
-
+console.log(process.env.XATA_DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.XATA_DATABASE_URL,
   max: 20,
